6d54db39c9254197d9688ce3b0a1621c8c447fd6
Kevin Flanagan <kevin@kflan.io>
2020-02-21 11:33:32 -0700

    readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,29 @@ import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
+const loadAuthenticatedApp = () => import('../components/authenticated-app');
+const AuthenticatedApp = React.lazy(loadAuthenticatedApp);
+const UnauthenticatedApp = React.lazy(() =>
+  import('../components/unauthenticated-app')
+);
+
 const IndexPage = () => {
+  // pre-load the authenticated side in the background while the user's
+  // filling out the login form.
+  React.useEffect(() => {
+    loadAuthenticatedApp();
+  }, []);
+
   const { isAuthenticated, user } = useAuth();
   return (
     <Layout>
+      <React.Suspense fallback={<>{/* spinner */}</>}>
+        {isAuthenticated() && <h1>Hi {user.email}</h1> ? (
+          <AuthenticatedApp />
+        ) : (
+          <UnauthenticatedApp />
+        )}
+      </React.Suspense>
       <SEO title="Home" />
       {!isAuthenticated() && <h1>Hi people</h1>}
       {isAuthenticated() && <h1>Hi {user.email}</h1>}
